2afe2e5a8cfc830785504bde46ad5d91a0d1fe88
ManashRaja <mpdmanash@gmail.com>
2018-03-11 18:28:09 +0530

    Add getKIbyResType in karlin_index.py

--- scripts/karlin_index.py
+++ scripts/karlin_index.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 sep = " "
 
@@ -25,6 +26,52 @@ def getNumResByName(residue_name, data_matrix):
 			counter = counter+1
 	return counter
 
+def getDistanceBRes(id1, id2, data_matrix):
+	dist = math.sqrt((data_matrix[id2][3]-data_matrix[id1][3])**2 + 
+				    (data_matrix[id2][4]-data_matrix[id1][4])**2 +
+				    (data_matrix[id2][5]-data_matrix[id1][5])**2)
+	return dist
+
+def getResFeature(residue_name):
+	hydro_list = ["GLY", "ALA", "VAL", "LEU", "ILE", "PRO", "PHE", "MET", "TRY"]
+	if residue_name in hydro_list:
+		return "HYDRO"
+
+def getFeatureFrequency(feature, data_matrix):
+	counter = 0
+	for i in range(len(data_matrix)):
+		if( getResFeature(data_matrix[i][1])  == feature ):
+			counter = counter +1
+	frequency = float(counter)/len(data_matrix)
+	return frequency
+
+def getNumFeatureTotalInNeighbor(ci, T, feature, data_matrix):
+	num_feature = 0
+	num_total = 0
+	for i in range(len(data_matrix)):
+		if i==ci:
+			continue
+		if(getDistanceBRes(ci, i, data_matrix) < T):
+			num_total = num_total+1
+			#check is residue belong to feature
+			if(getResFeature(data_matrix[i][1])  == feature):
+				num_feature = num_feature+1
+	return [num_feature, num_total]
+
+
+def getKIbyResType(residue_name, SS, SA, feature, frequency, T, data_matrix):
+	numerator = 0.0
+	denominator = 0.0
+	for i in range(len(data_matrix)):
+		#find residue by type
+		if(residue_name in data_matrix[i][1] and 
+		   SS == data_matrix[i][6] and
+		   SA == data_matrix[i][7]):
+			#in a radius T around it find num
+			#of residues of particular feature
+			[num_feature, num_total] = getNumFeatureTotalInNeighbor(i, T, feature, data_matrix)
+			print num_feature, num_total*frequency
+
 def main():
 	resp_filename = sys.argv[1]
 
@@ -32,7 +79,8 @@ def main():
 	resp_f = open(resp_filename,'r')
 	resp_data_lines = resp_f.readlines()
 	data_matrix = getDataMatrix(resp_data_lines)
-	print getNumResByName("ALA", data_matrix)
-	#print data_matrix
+	#print getNumResByName("ALA", data_matrix)
+	hydro_f = getFeatureFrequency("HYDRO", data_matrix)
+	getKIbyResType("ARG", "L", "I", "HYDRO", hydro_f, 5.0, data_matrix)
 
 main()
